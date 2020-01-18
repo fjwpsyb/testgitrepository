dbe7ac5c4031217cd1f6c335347fe0dd67594679
syb <350381204@qq.com>
2020-01-18 10:47:22 +0800

    测试代码冲突

--- src/main/java/com/syb/demo/TestGit.java
+++ src/main/java/com/syb/demo/TestGit.java
@@ -3,5 +3,6 @@ package com.syb.demo;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("上传到git");
+        System.out.println("test code 冲突");
     }
 }
