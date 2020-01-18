1121d154df2054fa22a94dcb544c2ba8b150cb87
syb <350381204@qq.com>
2020-01-18 10:57:22 +0800

    version3

--- src/main/java/com/syb/demo/TestGit.java
+++ src/main/java/com/syb/demo/TestGit.java
@@ -3,7 +3,6 @@ package com.syb.demo;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("上传到git");
-        System.out.println("更新代码上传到github");
-        System.out.println("test code 冲突");
+        System.out.println("test");
     }
 }
