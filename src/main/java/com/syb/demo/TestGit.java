e25823519f37a4111feb9073f8b83ba5ec7fee13
syb <350381204@qq.com>
2020-01-18 11:02:34 +0800

    Revert "version3"
    This reverts commit 1121d154

--- src/main/java/com/syb/demo/TestGit.java
+++ src/main/java/com/syb/demo/TestGit.java
@@ -3,6 +3,7 @@ package com.syb.demo;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("上传到git");
-        System.out.println("test");
+        System.out.println("更新代码上传到github");
+        System.out.println("test code 冲突");
     }
 }
