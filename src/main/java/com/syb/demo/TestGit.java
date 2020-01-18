3b001ef870662ea5ace8aae4278e267c5235c90c
syb <350381204@qq.com>
2020-01-18 10:41:44 +0800

    添加代码行

--- src/main/java/com/syb/demo/TestGit.java
+++ src/main/java/com/syb/demo/TestGit.java
@@ -3,5 +3,6 @@ package com.syb.demo;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("上传到git");
+        System.out.println("更新代码上传到github");
     }
 }
